16a8eaba2a29937acee3c2e8f51402540520dee8
Samuel Deng <samuel.deng@istuary.com>
2017-06-23 14:27:45 -0400

    up

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -4,26 +4,63 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
+import datetime
+import re
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
 
 
-class ArticlespiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# class ArticlespiderItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
+#
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+
+def get_nums(value):
+    try:
+        nums = int(re.findall(r'(\d+)', value)[0])
+    except IndexError:
+        nums = 0
+
+    return nums
+
+
+def tags_filter(value):
+    if "评论" not in value:
+        print(value)
+        return value
+
+
+# 自定义默认itemloader, 默认输出取数组的第一个值, 输出为字符串而不是数组
+class ArticleItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
 
 
 class JobBoleArticleItem(scrapy.Item):
     title = scrapy.Field()
-    create_date = scrapy.Field()
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert),
+    )
     url = scrapy.Field()
     url_object_id = scrapy.Field()
-    front_image_url = scrapy.Field()
+    front_image_url = scrapy.Field(
+        output_processor=MapCompose(lambda x: x)
+    )
     front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field()
-    comment_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
-    tags = scrapy.Field()
+    praise_nums = scrapy.Field(input_processor=MapCompose(get_nums))
+    comment_nums = scrapy.Field(input_processor=MapCompose(get_nums))
+    fav_nums = scrapy.Field(input_processor=MapCompose(get_nums))
+    tags = scrapy.Field(
+        input_processor=MapCompose(tags_filter),
+        output_processor=Join(",")
+    )
     content = scrapy.Field()
 
--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import Request
-from scrapy.loader import ItemLoader
 from urllib import parse
-import re
-import datetime
 
-from ArticleSpider.items import JobBoleArticleItem
-
+from ArticleSpider.items import JobBoleArticleItem, ArticleItemLoader
 from ArticleSpider.utils.common import get_md5
 
 
@@ -28,50 +24,59 @@ class JobboleSpider(scrapy.Spider):
         #     yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
 
     def parse_detail(self, response):
-        article_item = JobBoleArticleItem()
-
-        front_image_url = response.meta.get("front_image_url", "")  # Title images url
-        title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
-        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()[1]").extract()[0].strip().replace('·', '').strip()
-        try:
-            praise_nums = int(response.xpath("//div[@class='post-adds']/span/h10/text()").extract()[0])
-        except IndexError:
-            praise_nums = 0
-        try:
-            fav_nums = int(re.findall(r'(\d+)', response.xpath("//div[@class='post-adds']/span[2]/text()").extract()[0])[0])
-        except IndexError:
-            fav_nums = 0
-        try:
-            comment_nums = int(re.findall(r'(\d+)', response.xpath("//div[@class='post-adds']/a/span/text()").extract()[0])[0])
-        except IndexError:
-            comment_nums = 0
-        tag_list = list(set(response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()))
-        tags = ','.join(tag_list)
-        content = response.xpath("//div[@class='entry']").extract()[0]
-
-        article_item['url_object_id'] = get_md5(response.url)
-        article_item['title'] = title
-        article_item['url'] = response.url
-        try:
-            create_date = datetime.datetime.strptime(create_date, "%Y/%m/%d").date()
-        except Exception as e:
-            create_date = datetime.datetime.now().date()
-        article_item['create_date'] = create_date
-        article_item['front_image_url'] = [front_image_url]
-        article_item['praise_nums'] = praise_nums
-        article_item['fav_nums'] = fav_nums
-        article_item['comment_nums'] = comment_nums
-        article_item['tags'] = tags
-        article_item['content'] = content
 
         # 通过itemloader加载item
-        item_loader = ItemLoader(item=JobBoleArticleItem(), response=response)
-        item_loader.add_css()
+        item_loader = ArticleItemLoader(item=JobBoleArticleItem(), response=response)
+        front_image_url = response.meta.get("front_image_url", "")  # Title images url
+        # 本质上给属性传递一个list
         item_loader.add_xpath("title", "//div[@class='entry-header']/h1/text()")
         item_loader.add_xpath("create_date", "//p[@class='entry-meta-hide-on-mobile']/text()[1]")
         item_loader.add_xpath("praise_nums", "//div[@class='post-adds']/span/h10/text()")
         item_loader.add_xpath("fav_nums", "//div[@class='post-adds']/span[2]/text()")
-        item_loader.add_value()
+        item_loader.add_xpath("comment_nums", "//div[@class='post-adds']/a/span/text()")
+        item_loader.add_value("front_image_url", [front_image_url])
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("url_object_id", get_md5(response.url))
+        # 把tag_list里面重复的过滤掉, 在直接通过传值的方法传给"tags"
+        tag_list = list(set(response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()))
+        item_loader.add_value("tags", tag_list)
+        item_loader.add_xpath("content", "//div[@class='entry']")
 
+        article_item = item_loader.load_item()
 
         yield article_item
+
+
+        # article_item = JobBoleArticleItem()
+        # title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
+        # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()[1]").extract()[0].strip().replace('·', '').strip()
+        # try:
+        #     praise_nums = int(response.xpath("//div[@class='post-adds']/span/h10/text()").extract()[0])
+        # except IndexError:
+        #     praise_nums = 0
+        # try:
+        #     fav_nums = int(re.findall(r'(\d+)', response.xpath("//div[@class='post-adds']/span[2]/text()").extract()[0])[0]
+        # except IndexError:
+        #     fav_nums = 0
+        # try:
+        #     comment_nums = int(re.findall(r'(\d+)', response.xpath("//div[@class='post-adds']/a/span/text()").extract()[0])[0])
+        # except IndexError:
+        #     comment_nums = 0
+        # tag_list = list(set(response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()))
+        # tags = ','.join(tag_list)
+        # content = response.xpath("//div[@class='entry']").extract()[0]
+        #
+        # article_item['url_object_id'] = get_md5(response.url)
+        # article_item['title'] = title
+        # article_item['url'] = response.url
+        # try:
+        #     create_date = datetime.datetime.strptime(create_date, "%Y/%m/%d").date()
+        # except Exception as e:
+        #     create_date = datetime.datetime.now().date()
+        # article_item['create_date'] = create_date
+        # article_item['front_image_url'] = [front_image_url]
+        # article_item['praise_nums'] = praise_nums
+        # article_item['fav_nums'] = fav_nums
+        # article_item['comment_nums'] = comment_nums
+        # article_item['tags'] = tags
+        # article_item['content'] = content
